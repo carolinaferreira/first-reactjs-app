93b9af7d0222fc1cc55d01b7b27777aeb6246f24
carolinaferreira <carol.mf14@gmail.com>
2020-07-27 22:23:00 -0300

    :tada: inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.</h1>
+    </div>
+  );
+}
+
+export default App;
